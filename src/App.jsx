714d13520c0c16e7330893599227a7840cb70d87
sanoopark <sanoopark@gmail.com>
2021-11-29 18:41:13 +0900

    Chore: ThemeProvider 색상 변수 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = {
+  colors: {
+    brand: '#2A2E56',
+    normalWhite: '#ffffff',
+    normalBlack: '#2F3438',
+    normalRed: '#E05858',
+    normalPink: '#FF8585',
+    normalGreen: '#3BAB29',
+    normalGray: '#757575',
+    lightGray: '#ADADAD',
+    lighterGray: '#E5E5E5',
+    errorBg100: '#FEF2F2',
+    errorBg300: '#F87171',
+    errorBg500: '#B91C1C',
+    missing: '#E05858',
+    completion: '#53CE50',
+    sighting: '#FF8F5E',
+    protection: '#1E1AFF'
+  }
+};
 
 const App = function () {
-  return <></>;
+  return <ThemeProvider theme={theme}></ThemeProvider>;
 };
 
 export default App;
