de205f3e19953ab249e4cc8eb52e8daf192d84b1
wherehows <sensitive.hu@gmail.com>
2021-11-30 16:11:23 +0900

    Chore: Conflict 해결

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }]
   }
 };
