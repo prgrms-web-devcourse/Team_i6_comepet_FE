c0afee6ee622f287b926fe6d6f7833d801940284
wherehows <sensitive.hu@gmail.com>
2021-12-01 22:41:00 +0900

    Refactor: 코드 리뷰 반영
    - defaultProps 대신 단축 평가를 이용한 defaultValue 할당
    - as에 따라서 특정한 라벨 사용 가능
    - CSS 컨벤션 준수

--- src/components/StatusTag/StatusTag.jsx
+++ src/components/StatusTag/StatusTag.jsx
@@ -1,46 +1,70 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
+import { STATUS } from '@/utils/constants';
 
-const StatusTag = ({ children, padding, backgroundColor, width, fontSize, borderRadius }) => {
+const StatusTag = ({ as, children, padding, color, bgColor, width, fontSize, borderRadius }) => {
   return (
     <Wrapper>
       <StyledDiv
         padding={padding}
-        backgroundColor={backgroundColor}
+        bgColor={bgColor}
+        color={color}
         width={width}
         fontSize={fontSize}
         borderRadius={borderRadius}>
-        {children}
+        {decideText(as, bgColor, children)}
       </StyledDiv>
     </Wrapper>
   );
 };
 
-const StyledDiv = styled.div`
-  background-color: ${({ backgroundColor }) => backgroundColor};
-  padding: ${({ padding }) => padding};
-  width: ${({ width }) => width};
-  text-align: center;
-  font-size: ${({ fontSize }) => fontSize};
-  border-radius: ${({ borderRadius }) => borderRadius};
-`;
+const decideColor = ({ as, bgColor, theme }) => {
+  if (bgColor) {
+    return bgColor;
+  }
+
+  if (as) {
+    return theme.colors[STATUS[as]];
+  }
+
+  return theme.colors.lighterGray;
+};
+
+const decideText = (as, bgColor, children) => {
+  if (bgColor && children) {
+    return children;
+  }
+
+  if ((bgColor && !children) || (!bgColor && children)) return '배경색과 텍스트 둘 다 입력해주세요';
+
+  if (as) {
+    return STATUS[as];
+  }
+
+  return '내용을 입력해 주세요';
+};
 
 const Wrapper = styled.div``;
 
-StatusTag.defaultProps = {
-  children: '상태 없음',
-  padding: '0.6rem 0.6rem',
-  backgroundColor: '#ADADAD',
-  width: '5.6rem',
-  fontSize: '1.6rem',
-  borderRadius: '0 1.6rem 0 1.6rem'
-};
+const StyledDiv = styled.div`
+  background-color: ${(props) => decideColor(props)};
+  padding: ${({ padding }) => padding || '0.6rem'};
+  width: ${({ width }) => width || '5.6rem'};
+  border-radius: ${({ borderRadius }) => borderRadius || '0 1.6rem'};
+  font-size: ${({ fontSize }) => fontSize || '1.6rem'};
+  font-weight: bold;
+  text-align: center;
+  color: ${({ color, theme }) => color || theme.colors.normalWhite};
+`;
 
 StatusTag.propTypes = {
   children: PropTypes.string,
+  bgColor: PropTypes.string,
+  color: PropTypes.string,
+  as: PropTypes.string,
   padding: PropTypes.string,
-  backgroundColor: PropTypes.string,
+  height: PropTypes.string,
   width: PropTypes.string,
   fontSize: PropTypes.string,
   borderRadius: PropTypes.string
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,7 @@
 export const AUTH_ALERT = {};
+export const STATUS = Object.freeze({
+  missing: '실종',
+  completion: '완료',
+  sighting: '목격',
+  protection: '보호'
+});
