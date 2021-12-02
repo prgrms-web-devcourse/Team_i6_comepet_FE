794be51005d171ee6d6db975b57f8caf9ac90522
wherehows <sensitive.hu@gmail.com>
2021-12-02 13:44:43 +0900

    Feat: Input 마크업 추가

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,10 +1,61 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
 
-const Input = () => {
-  return <StyledInput></StyledInput>;
+const Input = ({
+  padding,
+  width,
+  height,
+  border,
+  borderRadius,
+  fontSize,
+  placeholder,
+  required
+}) => {
+  return (
+    <StyledInput
+      padding={padding}
+      width={width}
+      height={height}
+      border={border}
+      borderRadius={borderRadius}
+      fontSize={fontSize}
+      required={required}
+      placeholder={placeholder}></StyledInput>
+  );
 };
 
-const StyledInput = styled.input``;
+const StyledInput = styled.input`
+  width: ${({ width }) => width || '100%'};
+  height: ${({ height }) => height || '3rem'};
+  padding: ${({ padding }) => padding || '2rem'};
+  border: ${({ border }) => border || 'none'};
+  border-radius: ${({ borderRadius }) => borderRadius || '1rem'};
+  font-size: ${({ fontSize }) => fontSize || '1.6rem'};
+  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.08);
+  ::-ms-input-placeholder {
+    color: ${({ required, theme }) => (required && theme.colors.normalPink) || 'auto'};
+    font-weight: bold;
+  }
+  ::-webkit-input-placeholder {
+    color: ${({ required, theme }) => (required && theme.colors.normalPink) || 'auto'};
+    font-weight: bold;
+  }
+  ::-moz-placeholder {
+    color: ${({ required, theme }) => (required && theme.colors.normalPink) || 'auto'};
+    font-weight: bold;
+  }
+`;
+
+Input.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  borderRadius: PropTypes.string,
+  padding: PropTypes.string,
+  border: PropTypes.string,
+  fontSize: PropTypes.string,
+  required: PropTypes.bool
+};
 
 export default Input;
