1028ecb7cc2fc26a2a2a068328df6305b2815389
Park Sang-woo <sanoopark@gmail.com>
2021-12-02 19:06:07 +0900

    Feat: ScrapCounter 마크업 추가 (#21)
    * Feat: ScrapCounter 마크업 추가
    
    * Refactor: 잘못된 Prop 입력 시 에러 출력
    
    * Refactor: DEV_ERROR 객체 동결
    
    * Refactor: Flex Container를 span에서 div로 변경

---
+++ src/components/ScrapCounter/ScrapCounter.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+import StarIcon from '@mui/icons-material/Star';
+import { isNotValidSize } from '@/utils/helpers';
+import { DEV_ERROR } from '@/utils/constants';
+
+const ScrapCounter = ({ children, size }) => {
+  if (isNotValidSize(size, ['small', 'medium'])) {
+    console.error(DEV_ERROR.INVALID_PROP);
+    return;
+  }
+
+  return (
+    <Wrapper size={size}>
+      <StyledStarIcon size={size} />
+      <Counter size={size}>{children}</Counter>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: ${({ theme, size }) => theme.sizes.scrapCounter.wrapper[size].width};
+  height: ${({ theme, size }) => theme.sizes.scrapCounter.wrapper[size].height};
+  border-radius: 0.4rem 0 0 0;
+  padding: 0 0.4rem 0 0.2rem;
+  background-color: ${({ theme }) => theme.colors.normalBlack};
+  opacity: 90%;
+`;
+
+const StyledStarIcon = styled(StarIcon)`
+  width: ${({ theme, size }) => theme.sizes.scrapCounter.icon[size]};
+  height: ${({ theme, size }) => theme.sizes.scrapCounter.icon[size]};
+  color: #fecc00;
+`;
+
+const Counter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${({ theme, size }) => theme.sizes.scrapCounter.icon[size]};
+  height: ${({ theme, size }) => theme.sizes.scrapCounter.icon[size]};
+  margin-top: 5%;
+  font-size: ${({ theme, size }) => theme.sizes.scrapCounter.font[size]};
+  color: ${({ theme }) => theme.colors.normalWhite};
+`;
+
+ScrapCounter.propTypes = {
+  children: PropTypes.string.isRequired,
+  size: PropTypes.string.isRequired
+};
+
+export default ScrapCounter;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,5 @@
 export const AUTH_ALERT = {};
+
+export const DEV_ERROR = Object.freeze({
+  INVALID_PROP: '잘못된 Prop이 입력되었습니다.'
+});
