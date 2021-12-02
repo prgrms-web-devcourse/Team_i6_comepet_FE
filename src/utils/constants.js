e83cfd026d8c802d14d95b8a15768595b2405afb
wherehows <sensitive.hu@gmail.com>
2021-12-02 17:39:31 +0900

    Refactor: constants 폴더에 STATUS 네임 스페이스 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,9 @@
 export const AUTH_ALERT = {};
+
+export const DEV_ERROR = Object.freeze({
+  INVALID_PROP: '잘못된 Prop이 입력되었습니다.'
+});
+
 export const STATUS = Object.freeze({
   missing: '실종',
   completion: '완료',
