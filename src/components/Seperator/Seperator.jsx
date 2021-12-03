14400e9df141faf15b157d4f743e629a264a14ae
Jihyeon228 <lrsiyoor@gmail.com>
2021-12-03 16:10:36 +0900

    Feat: Seperator 마크업 추가

---
+++ src/components/Seperator/Seperator.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+
+const Seperator = ({ width, height, type }) => {
+  return <Wrapper type={type} width={width} height={height} />;
+};
+
+const Wrapper = styled.hr`
+  width: ${({ type }) => type === 'vertical' && '0.1rem'};
+  height: ${({ type, height }) => (type === 'horizon' && '0.1rem') || height};
+  color: ${({ theme }) => theme.colors.lighterGray};
+  border-bottom: 0;
+`;
+
+Seperator.defaultProps = {
+  children: <></>
+};
+
+Seperator.propTypes = {
+  type: PropTypes.node.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string
+};
+
+export default Seperator;
