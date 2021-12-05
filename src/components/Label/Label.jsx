17ea1205c58a53e52081a3bc3f39b867a6aa14a5
wherehows <sensitive.hu@gmail.com>
2021-12-05 11:26:11 +0900

    Refactor: Label 컴포넌트 수정
    - color, bgColor를 hexCode 컬러가 아닌 컬러 변수명으로 입력하도록 수정

--- src/components/Label/Label.jsx
+++ src/components/Label/Label.jsx
@@ -23,15 +23,15 @@ const Wrapper = styled.label`
   margin: ${({ margin }) => margin};
   padding: ${({ padding }) => padding || '0.6rem'};
   border-radius: ${({ borderRadius }) => borderRadius || '1.6rem'};
-  background-color: ${({ bgColor, theme }) => bgColor || theme.colors.lighterGray};
-  color: ${({ color, theme }) => color || theme.colors.normalWhite};
+  background-color: ${({ bgColor, theme }) => theme.colors[bgColor] || theme.colors.normalGray};
+  color: ${({ color, theme }) => theme.colors[color] || theme.colors.normalWhite};
   font-weight: bold;
 `;
 
 Label.propTypes = {
   children: PropTypes.node.isRequired,
-  forHtml: PropTypes.string.isRequired,
   bgColor: PropTypes.string.isRequired,
+  forHtml: PropTypes.string.isRequired,
   fontSize: PropTypes.string,
   margin: PropTypes.string,
   padding: PropTypes.string,
