19ddba4871b667625b054f48fa572af38fc467e7
wherehows <sensitive.hu@gmail.com>
2021-12-05 20:41:45 +0900

    Refactor: Label 컴포넌트 수정
    - color와 bgColor Prop hexCode가 아닌 컬러 변수명으로 받도록 설정

--- src/components/Label/Label.js
+++ src/components/Label/Label.js
@@ -23,8 +23,8 @@ const Wrapper = styled.label`
   margin: ${({ margin }) => margin};
   padding: ${({ padding }) => padding || '0.6rem'};
   border-radius: ${({ borderRadius }) => borderRadius || '1.6rem'};
-  background-color: ${({ bgColor, theme }) => bgColor || theme.colors.lighterGray};
-  color: ${({ color, theme }) => color || theme.colors.normalWhite};
+  background-color: ${({ bgColor, theme }) => theme.colors[bgColor] || theme.colors.lighterGray};
+  color: ${({ color, theme }) => theme.colors[color] || theme.colors.normalWhite};
   font-weight: bold;
 `;
 
