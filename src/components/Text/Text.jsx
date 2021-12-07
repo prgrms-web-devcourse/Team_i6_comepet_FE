d66d9628b06f11951fd89bff5b2419085f9d4e37
wherehows <sensitive.hu@gmail.com>
2021-12-07 13:35:52 +0900

    Refactor: Text 컴포넌트 수정
    - fontSize, fontWeight, cursor Prop 추가

--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 
-const Text = ({ children, color, margin }) => {
+const Text = ({ children, color, margin, fontSize, fontWeight, cursor }) => {
   return (
-    <Wrapper color={color} margin={margin}>
+    <Wrapper
+      color={color}
+      margin={margin}
+      fontSize={fontSize}
+      fontWeight={fontWeight}
+      cursor={cursor}>
       {children}
     </Wrapper>
   );
@@ -13,13 +18,18 @@ const Text = ({ children, color, margin }) => {
 const Wrapper = styled.span`
   display: inline-block;
   margin: ${({ margin }) => margin};
+  font-size: ${({ fontSize }) => fontSize};
+  font-weight: ${({ fontWeight }) => fontWeight};
   color: ${({ theme, color }) => theme.colors[color] || color};
-  font-weight: bold;
+  cursor: ${({ cursor }) => cursor};
 `;
 
 Text.propTypes = {
   color: PropTypes.string.isRequired,
   margin: PropTypes.string,
+  fontSize: PropTypes.string,
+  fontWeight: PropTypes.string,
+  cursor: PropTypes.string,
   children: PropTypes.string
 };
 
