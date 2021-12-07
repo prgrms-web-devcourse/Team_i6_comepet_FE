03c4a8d034b9c9e28cf43bedfbebbc4703ea3ae5
wherehows <sensitive.hu@gmail.com>
2021-12-07 13:33:38 +0900

    Refactor: helpers.js 파일 수정
    - getImageSrc 전역 함수 추가

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,6 +2,11 @@ export const isNotValidSize = (size, validSizes) => {
   return !validSizes.includes(size);
 };
 
+export const getImageSrc = (URL) => {
+  const { PUBLIC_URL } = process.env;
+  return `${PUBLIC_URL}${URL}`;
+};
+
 export const formatDate = (date) => {
   const DateObject = new Date(date);
   const year = DateObject.getFullYear();
