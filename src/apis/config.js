2ad0e6415153247973bae50224c386698efa08de
sanoopark <sanoopark@gmail.com>
2021-12-07 15:00:06 +0900

    Refactor: .env에서 Content-Type 제거

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-const CONTENT_TYPE = process.env.REACT_APP_CONTENT_TYPE;
+const CONTENT_TYPE = 'application/json';
 const AUTH_TOKEN = ''; // TODO
 
 axios.defaults.baseURL = BASE_URL;
