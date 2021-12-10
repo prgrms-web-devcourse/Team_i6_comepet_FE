5ceca8a403140e82401660c39e66d2aa4f32cc4f
sanoopark <sanoopark@gmail.com>
2021-12-10 20:13:43 +0900

    Refactor: MainPage 더미 데이터 추가

---
+++ src/assets/data.js
@@ -0,0 +1,184 @@
+export const postsData = {
+  posts: [
+    {
+      id: 1,
+      city: '서울특별시',
+      town: '도봉구',
+      animalKind: '토이푸들',
+      status: 'DETECTION',
+      createdAt: '2021-11-05T16:55:37.436056',
+      sex: 'FEMALE',
+      isBookmark: true,
+      bookmarkCount: 2,
+      tags: [
+        {
+          id: 1,
+          name: '가나다'
+        },
+        {
+          id: 2,
+          name: '라마바'
+        },
+        {
+          id: 3,
+          name: '사아자'
+        },
+        {
+          id: 4,
+          name: '차카타파하'
+        }
+      ],
+      thumbnail: null
+    },
+    {
+      id: 2,
+      city: '서울특별시',
+      town: '도봉구',
+      animalKind: '포메라니안',
+      status: 'PROTECTION',
+      createdAt: '2021-11-05T16:55:37.436056',
+      sex: 'MALE',
+      isBookmark: false,
+      bookmarkCount: 2,
+      tags: [
+        {
+          id: 1,
+          name: '가나다'
+        },
+        {
+          id: 2,
+          name: '라마바'
+        },
+        {
+          id: 3,
+          name: '사아자'
+        },
+        {
+          id: 4,
+          name: '차카타파하'
+        }
+      ],
+      thumbnail: null
+    },
+    {
+      id: 3,
+      city: '서울특별시',
+      town: '도봉구',
+      animalKind: 'UNKNOWN',
+      status: 'MISSING',
+      createdAt: '2021-11-05T16:55:37.436056',
+      sex: 'MALE',
+      isBookmark: false,
+      bookmarkCount: 2,
+      tags: [
+        {
+          id: 1,
+          name: '가나다'
+        },
+        {
+          id: 2,
+          name: '라마바'
+        },
+        {
+          id: 3,
+          name: '사아자'
+        },
+        {
+          id: 4,
+          name: '차카타파하'
+        }
+      ],
+      thumbnail: null
+    },
+    {
+      id: 4,
+      city: '서울특별시',
+      town: '도봉구',
+      animalKind: 'UNKNOWN',
+      status: 'DETECTION',
+      createdAt: '2021-11-05T16:55:37.436056',
+      sex: 'MALE',
+      isBookmark: false,
+      bookmarkCount: 2,
+      tags: [
+        {
+          id: 1,
+          name: '가나다'
+        },
+        {
+          id: 2,
+          name: '라마바'
+        },
+        {
+          id: 3,
+          name: '사아자'
+        },
+        {
+          id: 4,
+          name: '차카타파하'
+        }
+      ],
+      thumbnail: null
+    },
+    {
+      id: 5,
+      city: '서울특별시',
+      town: '도봉구',
+      animalKind: 'UNKNOWN',
+      status: 'PROTECTION',
+      createdAt: '2021-11-05T16:55:37.436056',
+      sex: 'MALE',
+      isBookmark: false,
+      bookmarkCount: 2,
+      tags: [
+        {
+          id: 1,
+          name: '가나다'
+        },
+        {
+          id: 2,
+          name: '라마바'
+        },
+        {
+          id: 3,
+          name: '사아자'
+        },
+        {
+          id: 4,
+          name: '차카타파하'
+        }
+      ],
+      thumbnail: null
+    },
+    {
+      id: 6,
+      city: '서울특별시',
+      town: '도봉구',
+      animalKind: 'UNKNOWN',
+      status: 'COMPLETION',
+      createdAt: '2021-11-05T16:55:37.436056',
+      sex: 'MALE',
+      isBookmark: false,
+      bookmarkCount: 2,
+      tags: [
+        {
+          id: 1,
+          name: '가나다'
+        },
+        {
+          id: 2,
+          name: '라마바'
+        },
+        {
+          id: 3,
+          name: '사아자'
+        },
+        {
+          id: 4,
+          name: '차카타파하'
+        }
+      ],
+      thumbnail: null
+    }
+  ]
+};
