068de692c49c355c751a9588894a2bae3c9f329e
Park Sang-woo <sanoopark@gmail.com>
2021-12-10 20:46:36 +0900

    Feat: LoginPage 마크업 추가 (#57)
    * Feat: FoldingForm 마크업 추가
    
    * Feat: SocialLink 마크업 추가
    
    * Chore: Conflict 해결
    
    * Refactor: Theme 색상 추가
    
    * Refactor: constants USER_ERROR 객체 값 추가
    
    * Refactor: helpers isValidInput 함수 추가
    
    * Feat: LoginPage 마크업 추가
    
    * Refactor: LoginPage ShortHeader 경로 수정
    
    * Refactor: LoginPage Container 네이밍 변경

---
+++ src/views/Auth/LoginPage/SocialLink/SocialLink.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+import { getImageSrc } from '@/utils/helpers';
+import { Image } from '@/components/Image';
+import { DEV_ERROR } from '@/utils/constants';
+
+const SocialLink = ({ type }) => {
+  const imageSrc = getImageUrl(type);
+  const bgColor = getBgColor(type);
+
+  return (
+    <Link href="#" bgColor={bgColor} type={type}>
+      <Image src={imageSrc} alt={type} width="auto" height="1.8rem" />
+    </Link>
+  );
+};
+
+/* TODO: react-router의 Link로 교체 */
+const Link = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+  background-color: ${({ bgColor }) => bgColor};
+  border: ${({ type, theme }) => type === 'google' && `0.1rem solid ${theme.colors.lighterGray}`};
+  margin: 0 4%;
+`;
+
+SocialLink.propTypes = {
+  type: PropTypes.string
+};
+
+export default SocialLink;
+
+const getImageUrl = (type) => {
+  let url = '';
+
+  switch (type) {
+    case 'google':
+      url = '/images/social-logo-google.svg';
+      break;
+    case 'kakao':
+      url = '/images/social-logo-kakao.svg';
+      break;
+    case 'naver':
+      url = '/images/social-logo-naver.svg';
+      break;
+    default:
+      console.error(DEV_ERROR.INVALID_PROP);
+  }
+
+  return getImageSrc(url);
+};
+
+const getBgColor = (type) => {
+  let bgColor = '';
+
+  switch (type) {
+    case 'google':
+      bgColor = '#f8f8f8';
+      break;
+    case 'kakao':
+      bgColor = '#fdec03';
+      break;
+    case 'naver':
+      bgColor = '#30c739';
+      break;
+    default:
+      console.error(DEV_ERROR.INVALID_PROP);
+  }
+
+  return getImageSrc(bgColor);
+};
