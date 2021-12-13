55d841c687a2e6155611d9a4d290a962e33910f1
wherehows <sensitive.hu@gmail.com>
2021-12-13 23:20:45 +0900

    Refactor: Status 카테고리 추가

--- src/views/Post/PostCreatePage/Category/Status/Status.jsx
+++ src/views/Post/PostCreatePage/Category/Status/Status.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import LineBreakWrapper from '../Common/LineBreakWrapper';
 import { Label } from '@/components/Label';
 import { SelectionBox } from '@/components/SelectionBox';
 
-const Status = () => {
+const Status = ({ onChange }) => {
+  const handleChange = (e) => {
+    if (isDefalutOptionSelected(e)) {
+      onChange(e);
+    } else {
+      onChange({ target: { name: 'status', value: null } });
+    }
+  };
+
   return (
     <Wrapper>
       <Label forHtml="status" bgColor="brand">
@@ -13,6 +22,8 @@ const Status = () => {
       <LineBreakWrapper margin="1.8rem 0 0 0">
         <SelectionBox
           id="status"
+          name="status"
+          onChange={handleChange}
           options={['실종', '목격', '발견', '완료']}
           defaultOption="상태 옵션"
           required={true}
@@ -24,4 +35,9 @@ const Status = () => {
 
 const Wrapper = styled.div``;
 
+Status.propTypes = {
+  onChange: PropTypes.func
+};
+
 export default Status;
+const isDefalutOptionSelected = (e) => e.target[0].textContent === e.target.value;
