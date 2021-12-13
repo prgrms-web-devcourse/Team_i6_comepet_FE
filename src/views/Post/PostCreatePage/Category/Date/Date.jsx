fcbd2f8d1e02e4165b6daaa2886cf622085a769f
wherehows <sensitive.hu@gmail.com>
2021-12-13 23:15:52 +0900

    Refactor: Date 카테고리 수정
    - 코드 리펙토링
    - SelectionBox를 선택하지 않는 경우에 대한 null값 할당 추가

--- src/views/Post/PostCreatePage/Category/Date/Date.jsx
+++ src/views/Post/PostCreatePage/Category/Date/Date.jsx
@@ -5,33 +5,26 @@ import LineBreakWrapper from '../Common/LineBreakWrapper';
 import { Label } from '@/components/Label';
 import { SelectionBox } from '@/components/SelectionBox';
 
-// TODO:
-// 2020년 12월 30일에서 년도를 2021년으로 바꾸면 색깔이 초기화되지 않는 버그
-
 const Date = ({ margin, onChange }) => {
   const [selectedYear, setSelectedYear] = useState(null);
   const [selectedMonth, setSelectedMonth] = useState(null);
   const [selectedDay, setSelectedDay] = useState(null);
 
-  const handleChange = ({ target }) => {
-    if (target[0].textContent === '년') {
-      Number.isNaN(Number(target.value))
-        ? setSelectedYear(null)
-        : setSelectedYear(Number(target.value));
+  const handleChange = (e) => {
+    if (isYearSelection(e)) {
+      isDefalutOptionSelected(e) ? setSelectedYear(null) : setSelectedYear(Number(e.target.value));
       return;
     }
 
-    if (target[0].textContent === '월') {
-      Number.isNaN(Number(target.value))
+    if (isMonthSelection(e)) {
+      isDefalutOptionSelected(e)
         ? setSelectedMonth(null)
-        : setSelectedMonth(Number(target.value));
+        : setSelectedMonth(Number(e.target.value));
       return;
     }
 
-    if (target[0].textContent === '일') {
-      Number.isNaN(Number(target.value))
-        ? setSelectedDay(null)
-        : setSelectedDay(Number(target.value));
+    if (isDaySelection(e)) {
+      isDefalutOptionSelected(e) ? setSelectedDay(null) : setSelectedDay(Number(e.target.value));
       return;
     }
   };
@@ -51,33 +44,37 @@ const Date = ({ margin, onChange }) => {
       onChange({
         target: { name: 'date', value: `${selectedYear}-${selectedMonth}-${selectedDay}` }
       });
+    } else {
+      onChange({
+        target: { name: 'date', value: null }
+      });
     }
   }, [selectedYear, selectedMonth, selectedDay]);
 
   return (
     <Wrapper margin={margin}>
-      <Label forHtml="status" bgColor="brand">
+      <Label forHtml="year" bgColor="brand">
         날짜
       </Label>
       <LineBreakWrapper margin="1.8rem 0 0 0">
         <SelectionBox
-          id="status"
-          onSelectChange={handleChange}
+          id="year"
+          onChange={handleChange}
           options={getRangeOfYear()}
           defaultOption="년"
           required={true}
         />
         <SelectionBox
-          id="status"
-          onSelectChange={handleChange}
+          id="month"
+          onChange={handleChange}
           options={getRangeOfMonth(selectedYear)}
           defaultOption="월"
           required={true}
           margin="0 0 0 2rem"
         />
         <SelectionBox
-          id="status"
-          onSelectChange={handleChange}
+          id="day"
+          onChange={handleChange}
           options={getRangeOfDay(selectedMonth, selectedYear)}
           defaultOption="일"
           required={true}
@@ -165,4 +162,8 @@ const isLeapYear = (year) => {
 
 const isMonthThatHas31Days = (month) => [1, 3, 5, 7, 8, 10, 12].includes(month);
 const isMonthThatHas30Days = (month) => [4, 6, 9, 11].includes(month);
-const February = '2';
+const isYearSelection = (e) => e.target[0].textContent === '년';
+const isMonthSelection = (e) => e.target[0].textContent === '월';
+const isDaySelection = (e) => e.target[0].textContent === '일';
+const isDefalutOptionSelected = (e) => e.target[0].textContent === e.target.value;
+const February = 2;
