8cb687d02086c5c175f386947e6bc22f6db8af39
Park Sang-woo <sanoopark@gmail.com>
2021-12-13 20:41:40 +0900

    Feat: URL 라우팅 설정 (#76)
    * Chore: react-router 설치
    
    * Refactor: App Theme 객체 분리 및 ThemeProvider 위치 변경
    
    * Feat: URL 라우팅 설정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '@/utils/theme';
 import App from '@/App';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
