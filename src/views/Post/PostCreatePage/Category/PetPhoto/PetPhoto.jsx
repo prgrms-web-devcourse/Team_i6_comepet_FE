e256231af084153f9084dfdc31801ebd87feb82e
wherehows <sensitive.hu@gmail.com>
2021-12-13 23:17:42 +0900

    Feat: PetPhoto 카테고리 추가

---
+++ src/views/Post/PostCreatePage/Category/PetPhoto/PetPhoto.jsx
@@ -0,0 +1,51 @@
+import React, { useRef, useState } from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+import { Slider } from '@/components/Slider';
+import { Button } from '@/components/Button';
+
+const PetPhoto = ({ margin }) => {
+  const [files, setFiles] = useState([]);
+
+  const inputRef = useRef(null);
+
+  const handleChooseFile = () => {
+    inputRef.current.click();
+  };
+
+  const handleFileChange = (e) => {
+    const uploadedFiles = [...e.target.files];
+    const nextFiles = [...files];
+    nextFiles.push(...uploadedFiles);
+    setFiles(nextFiles);
+  };
+
+  return (
+    <Wrapper margin={margin}>
+      <Slider imageList={files} size="large" />
+      <Input onChange={handleFileChange} ref={inputRef} type="file" accept="image/*" multiple />
+      <Button
+        onClick={handleChooseFile}
+        width="60%"
+        margin="5% auto 0 auto"
+        bgColor="normalOrange"
+        type="button">
+        반려동물 사진 등록
+      </Button>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  margin: ${({ margin }) => margin};
+`;
+
+const Input = styled.input`
+  display: none;
+`;
+
+PetPhoto.propTypes = {
+  margin: PropTypes.string
+};
+
+export default PetPhoto;
