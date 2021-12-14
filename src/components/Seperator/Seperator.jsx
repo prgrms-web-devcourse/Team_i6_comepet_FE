8efeea8aece6648f943a550e29bd0558d38101cb
Jihyeon228 <lrsiyoor@gmail.com>
2021-12-14 15:41:13 +0900

    Fix: bgColor prop 추가

--- src/components/Seperator/Seperator.jsx
+++ src/components/Seperator/Seperator.jsx
@@ -2,23 +2,24 @@ import React from 'react';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 
-const Seperator = ({ width, height, type, margin }) => {
-  return <Wrapper type={type} width={width} height={height} margin={margin} />;
+const Seperator = ({ width, height, type, margin, bgColor }) => {
+  return <Wrapper type={type} width={width} height={height} margin={margin} bgColor={bgColor} />;
 };
 
 const Wrapper = styled.hr`
-  width: ${({ type, width }) => type === 'vertical' && (width || '0.05rem')};
-  height: ${({ type, height }) => type === 'horizon' && (height || '0.05rem')};
-  background-color: ${({ theme }) => theme.colors.lighterGray};
+  width: ${({ type, width }) => (type === 'vertical' && (width || '0.05rem')) || width};
+  height: ${({ type, height }) => (type === 'horizon' && (height || '0.05rem')) || height};
+  background-color: ${({ theme, bgColor }) => theme.colors[bgColor] || theme.colors.lighterGray};
   margin: ${({ margin }) => margin || '0'};
-  border: none;
+  border: 0;
 `;
 
 Seperator.propTypes = {
   type: PropTypes.node.isRequired,
   width: PropTypes.string,
   height: PropTypes.string,
-  margin: PropTypes.string
+  margin: PropTypes.string,
+  bgColor: PropTypes.string
 };
 
 export default Seperator;
