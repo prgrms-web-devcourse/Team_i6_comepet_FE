c7606f5d4fd1206d3c896c9f1907780135c4617c
sanoopark <sanoopark@gmail.com>
2021-12-18 01:54:11 +0900

    Feat: MainPage 무한스크롤 구현

--- src/views/Main/MainPage/MainPage.jsx
+++ src/views/Main/MainPage/MainPage.jsx
@@ -1,53 +1,66 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from '@emotion/styled';
+import useSWRInfinite from 'swr/infinite';
+import { useInView } from 'react-intersection-observer';
 import { LongHeader } from '@/components/Header';
-import { PostCard } from '@/components/PostCard';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { SortHeader } from '@/views/Main/SortHeader';
-import useSWR from 'swr';
+import { PostCard } from '@/components/PostCard';
+import { Button } from '@/components/Button';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { GET } from '@/apis/axios';
+import { DEV_ERROR } from '@/utils/constants';
 
 const MainPage = () => {
-  const { data } = useSWR('/missing-posts', (url) =>
-    GET(url, {
-      page: 1,
-      size: 30
-    })
+  const [target, targetInView] = useInView();
+  const { data, error, size, setSize } = useSWRInfinite(
+    (index) => `/missing-posts?page=${index + 1}&size=6`,
+    GET
   );
 
-  if (!data) {
-    return (
-      <Wrapper>
-        <LongHeader />
-      </Wrapper>
-    );
-  }
+  error && alert(DEV_ERROR.LOAD_FAILED);
 
-  const posts = data.missingPosts;
-  const postLength = posts.length || 0;
-  const city = posts[0].city;
-  const town = posts[0].town;
+  const posts = data?.reduce((prevData, nextData) => {
+    return { missingPosts: [...prevData.missingPosts, ...nextData.missingPosts] };
+  }).missingPosts;
+
+  const isReachingEnd = data && data[data?.length - 1]?.last;
+  const city = posts && posts[0]?.city; // temp
+  const town = posts && posts[0]?.town; // temp
+  const postLength = posts?.length || 0;
+
+  useEffect(() => {
+    targetInView && setSize(size + 1);
+  }, [targetInView]);
 
   return (
     <Wrapper>
       <LongHeader />
       <ContentWrapper>
-        <SortHeader city={city} town={town} postLength={postLength} />
+        <SortHeader city={city || '전체'} town={town || ''} postLength={postLength} />
         {postLength ? (
           <PostCardList>
             {posts.map(({ id, ...props }) => (
               <PostCardWrapper key={id}>
-                <PostCard {...props} />
+                <PostCard postId={id} {...props} />
               </PostCardWrapper>
             ))}
           </PostCardList>
         ) : (
           <NoResultText>검색 결과가 없습니다.</NoResultText>
         )}
+        <Button
+          width="50%"
+          margin="6rem auto"
+          bgColor="brand"
+          disabled={isReachingEnd}
+          onClick={() => setSize(size + 1)}>
+          {(isReachingEnd && '마지막') || '더보기'}
+        </Button>
       </ContentWrapper>
       <Link>
         <StyledAddCircleIcon />
       </Link>
+      <div ref={target}></div>
     </Wrapper>
   );
 };
@@ -84,6 +97,7 @@ const Link = styled.button`
   position: fixed;
   right: 0;
   bottom: 0;
+  z-index: 2;
   transform: translateX(-50%);
   display: flex;
   justify-content: center;
