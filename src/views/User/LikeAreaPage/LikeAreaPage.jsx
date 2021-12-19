ed59bf58a69f5c81a4b8a3597d9ea88d4c6a0bdb
Jihyeon228 <lrsiyoor@gmail.com>
2021-12-19 15:40:52 +0900

    Feat: LikeArea 조회, 수정, 삭제 기능 추가

--- src/views/User/LikeAreaPage/LikeAreaPage.jsx
+++ src/views/User/LikeAreaPage/LikeAreaPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import ToggleOffIcon from '@mui/icons-material/ToggleOff';
 import ToggleOnIcon from '@mui/icons-material/ToggleOn';
@@ -6,13 +6,57 @@ import AddIcon from '@mui/icons-material/Add';
 import { ShortHeader } from '@/components/Header';
 import { Label } from '@/components/Label';
 import { Seperator } from '@/components/Seperator';
-import { BackgroundBox } from '@/components/BackgroundBox';
 import { Button } from '@/components/Button';
 import LikeAreaModal from './LikeAreaModal';
+import { GET, PUT, DELETE } from '@/apis/axios';
+import useSWR from 'swr';
+import LikeAreaButton from './LikeAreaButton';
 
 const LikeAreaPage = () => {
+  const { data: user } = useSWR('/me/areas', GET);
+  const { data: placeData } = useSWR('/cities', GET);
+  const [initialInterestAreas, setInitialInterestAreas] = useState([]);
   const [isToggled, setIsToggled] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
+  const [isDefaultArea, setIsDefaultArea] = useState(true);
+
+  useEffect(() => {
+    setIsToggled(user?.checked);
+    setInitialInterestAreas(user?.areas);
+    initialInterestAreas?.length > 0 && setIsDefaultArea(false);
+  }, [user]);
+
+  const handleOnClick = () => {
+    initialInterestAreas?.length > 0 && setIsDefaultArea(false);
+    setIsVisible(true);
+  };
+
+  const handleOnDelete = async (e) => {
+    const deleteId = e.target.dataset.id;
+    await DELETE(`/me/areas/${deleteId}`);
+    alert('관심지역이 삭제되었습니다');
+  };
+
+  const getSelectedTown = async (selectedTown) => {
+    setInitialInterestAreas((initialInterestAreas) => {
+      if (initialInterestAreas.length > 1) {
+        return initialInterestAreas;
+      } else {
+        return [...initialInterestAreas, selectedTown];
+      }
+    });
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    const selectedAreas = initialInterestAreas?.map(({ townId, defaultArea }) => {
+      return { townId, defaultArea };
+    });
+    const save = { areas: selectedAreas, notification: isToggled };
+    await PUT('/me/areas', save);
+    alert('관심지역이 등록되었습니다');
+  };
+
   return (
     <Wrapper>
       <ShortHeader location="관심 지역 설정" />
@@ -23,35 +67,69 @@ const LikeAreaPage = () => {
           </Label>
           <Seperator type="horizon" width="100%" bgColor="lightGray" />
         </LabelWrapper>
-        <SwitchWrapper>
-          <NoticeText>알림을 켜면, 관심 지역의 소식을 바로 확인할 수 있습니다.</NoticeText>
-          <Button
-            margin="0 4rem 0 1rem"
-            bgColor="normalWhite"
-            height="2rem"
-            width="3.4rem"
-            onClick={() => {
-              setIsToggled(!isToggled);
-            }}>
-            {isToggled ? <StyledToggleOnIcon /> : <StyledToggleOffIcon />}
+        <Form onSubmit={handleOnSubmit}>
+          <SwitchWrapper>
+            <NoticeText>알림을 켜면, 관심 지역의 소식을 바로 확인할 수 있습니다.</NoticeText>
+            <Button
+              margin="0 4rem 0 1rem"
+              bgColor="normalWhite"
+              height="2rem"
+              width="3.4rem"
+              onClick={() => {
+                setIsToggled(!isToggled);
+              }}>
+              {isToggled ? <StyledToggleOnIcon /> : <StyledToggleOffIcon />}
+            </Button>
+          </SwitchWrapper>
+          <SelectedAreaWrapper>
+            {initialInterestAreas?.length === 0 ? (
+              <LikeAreaButton onClick={handleOnClick}>
+                <StyledAddIcon />
+              </LikeAreaButton>
+            ) : initialInterestAreas?.length === 1 ? (
+              initialInterestAreas?.map(({ id, townName, defaultArea }, index) => {
+                return (
+                  <React.Fragment key={index}>
+                    <LikeAreaButton
+                      dataNo={id}
+                      isDefaultArea={defaultArea}
+                      initialInterestAreas={initialInterestAreas}
+                      onDelete={handleOnDelete}>
+                      {townName}
+                    </LikeAreaButton>
+                    <LikeAreaButton onClick={handleOnClick}>
+                      <StyledAddIcon />
+                    </LikeAreaButton>
+                  </React.Fragment>
+                );
+              })
+            ) : (
+              initialInterestAreas?.map(({ id, townName, defaultArea }, index) => {
+                return (
+                  <LikeAreaButton
+                    key={index}
+                    dataNo={id}
+                    isDefaultArea={defaultArea}
+                    initialInterestAreas={initialInterestAreas}
+                    onDelete={handleOnDelete}>
+                    {townName}
+                  </LikeAreaButton>
+                );
+              })
+            )}
+          </SelectedAreaWrapper>
+          <Button type="submit" width="60%" height="4rem" bgColor="normalOrange" margin="auto">
+            저장하기
           </Button>
-        </SwitchWrapper>
-        <SelectedAreaWrapper>
-          <BackgroundBox width="40%" height="4rem" boxShadow="light" borderRadius="0">
-            <Button bgColor="normalWhite" borderRadius="0" onClick={() => setIsVisible(true)}>
-              <StyledAddIcon />
-            </Button>
-          </BackgroundBox>
-          <BackgroundBox width="40%" height="4rem" boxShadow="light" borderRadius="0">
-            <Button bgColor="normalWhite" borderRadius="0" onClick={() => setIsVisible(true)}>
-              <StyledAddIcon />
-            </Button>
-          </BackgroundBox>
-        </SelectedAreaWrapper>
-        <Button width="60%" height="4rem" bgColor="normalOrange" margin="auto">
-          저장하기
-        </Button>
-        {isVisible && <LikeAreaModal onClose={() => setIsVisible(false)} />}
+          {isVisible && (
+            <LikeAreaModal
+              placeData={placeData}
+              checkOut={getSelectedTown}
+              onClose={() => setIsVisible(false)}
+              isDefaultArea={isDefaultArea}
+            />
+          )}
+        </Form>
       </LikeAreaWrapper>
     </Wrapper>
   );
@@ -78,6 +156,12 @@ const LabelWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex-grow: 1;
 `;
 const SwitchWrapper = styled.div`
   display: flex;
