d5ddd96f00f8c380ca4d5daae7bf949843559e2e
wherehows <sensitive.hu@gmail.com>
2021-12-19 13:53:18 +0900

    Refactor: AuthProvider 수정
    - isLoggedIn, userId state 추가

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,23 +1,23 @@
 import React, { useEffect, useState, createContext } from 'react';
 import PropTypes from 'prop-types';
 import { GET } from '@/apis/axios';
-import { getCookie } from '@/utils/cookie';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const value = { isLoggedIn, setIsLoggedIn };
+  const [userId, setUserId] = useState(null);
+  const value = { isLoggedIn, setIsLoggedIn, userId };
 
   useEffect(() => {
-    const userToken = getCookie('token');
-
     const getAuthStatus = async () => {
-      const userIdData = await GET('/auth-user');
-      userIdData && setIsLoggedIn(true);
+      const userAuthData = await GET('/auth-user');
+      const userAuthId = userAuthData?.id;
+      userAuthId && setIsLoggedIn(true);
+      setUserId(userAuthId);
     };
 
-    userToken && getAuthStatus();
+    getAuthStatus();
   }, []);
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
