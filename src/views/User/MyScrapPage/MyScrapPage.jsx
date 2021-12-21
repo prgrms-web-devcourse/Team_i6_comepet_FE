06a4dbd37f4194bfcb98f7b75ff3bee559505101
Jihyeon228 <lrsiyoor@gmail.com>
2021-12-21 22:51:45 +0900

    Refactor: 코드리뷰반영

--- src/views/User/MyScrapPage/MyScrapPage.jsx
+++ src/views/User/MyScrapPage/MyScrapPage.jsx
@@ -11,50 +11,33 @@ import { PostCard } from '@/components/PostCard';
 const MyScrapPage = () => {
   const [status, setStatus] = useState('missing');
   const [target, isTargetInView] = useInView();
-  const [isMissingActive, setIsMissingActive] = useState(true);
-  const [isShelterActive, setIsShelterActive] = useState(false);
-
-  const handleButton = ({ target }) => {
-    if (target.id === 'missing') {
-      setStatus('missing');
-      setIsMissingActive(true);
-      setIsShelterActive(false);
-    } else if (target.id === 'shelter') {
-      setStatus('shelter');
-      setIsMissingActive(false);
-      setIsShelterActive(true);
-    }
-  };
-
   const { data, size, setSize } = useSWRInfinite(
-    (index) => `/me/bookmarks?status=${status}?page=${index}&size=10`,
+    (index) => `/me/bookmarks?status=${status}&page=${index}&size=10`,
     GET
   );
 
   const posts = data?.reduce((prevData, nextData) => {
-    return { posts: [...prevData.posts, ...nextData.posts] };
-  }).posts;
+    return { posts: [...prevData?.posts, ...nextData?.posts] };
+  })?.posts;
 
   const isReachingEnd = data && data[data?.length - 1]?.last;
-
   const postLength = posts?.length || 0;
 
   useEffect(() => {
     if (!isReachingEnd && isTargetInView) {
-      isTargetInView && setSize(size + 1);
+      setSize(size + 1);
     }
   }, [isTargetInView]);
 
-  if (!data) {
-    return (
-      <Wrapper>
-        <ShortHeader location="내가 저장한 글" />
-        <ContentWrapper>
-          <ButtonWrapper />
-        </ContentWrapper>
-      </Wrapper>
-    );
-  }
+  const handleButton = ({ target }) => {
+    const TYPE = target.id;
+
+    if (TYPE === 'missing') {
+      setStatus('missing');
+    } else if (TYPE === 'shelter') {
+      setStatus('shelter');
+    }
+  };
 
   return (
     <Wrapper>
@@ -68,7 +51,7 @@ const MyScrapPage = () => {
             height="2.8rem"
             fontSize="1rem"
             fontWeight="bold"
-            bgColor={isMissingActive ? 'brand' : 'lightGray'}
+            bgColor={status === 'missing' ? 'brand' : 'lightGray'}
             onClick={handleButton}>
             실종 / 보호
           </Button>
@@ -79,49 +62,34 @@ const MyScrapPage = () => {
             height="2.8rem"
             fontSize="1rem"
             fontWeight="bold"
-            bgColor={isShelterActive ? 'brand' : 'lightGray'}
+            bgColor={status === 'shelter' ? 'brand' : 'lightGray'}
             onClick={handleButton}>
             보호소
           </Button>
         </ButtonWrapper>
-        <MissingWrapper isMissingActive={isMissingActive}>
-          {postLength ? (
-            <PostCardList>
-              {posts.map(({ id, animalKind, sexType, ...props }) => (
-                <Link to={`/post/${id}`} key={id}>
-                  <PostCardWrapper>
-                    <PostCard animalKindName={animalKind} sex={sexType} {...props} />
-                  </PostCardWrapper>
-                </Link>
-              ))}
-            </PostCardList>
-          ) : (
-            <NoResultText>저장한 실종/보호 게시글이 없습니다.</NoResultText>
-          )}
-        </MissingWrapper>
-        <ShelterWrapper isShelterActive={isShelterActive}>
-          {postLength ? (
-            <PostCardList>
-              {posts.map(({ id, animalKind, sexType, ...props }) => (
-                <Link to={`/shelter/${id}`} key={id}>
-                  <PostCardWrapper>
-                    <PostCard animalKindName={animalKind} sex={sexType} {...props} />
-                  </PostCardWrapper>
-                </Link>
-              ))}
-            </PostCardList>
-          ) : (
-            <NoResultText>저장한 보호소 게시글이 없습니다.</NoResultText>
-          )}
-        </ShelterWrapper>
-        <Button
-          width="50%"
-          margin="6rem auto"
-          bgColor="brand"
-          disabled={isReachingEnd}
-          onClick={() => setSize(size + 1)}>
-          {(isReachingEnd && '마지막') || '더보기'}
-        </Button>
+        {postLength ? (
+          <PostCardList>
+            {posts.map(({ id, animalKind, sexType, ...props }) => (
+              <Link to={`/post/${id}`} key={id}>
+                <PostCardWrapper>
+                  <PostCard animalKindName={animalKind} sex={sexType} {...props} />
+                </PostCardWrapper>
+              </Link>
+            ))}
+          </PostCardList>
+        ) : (
+          <NoResultText>저장한 실종/보호 게시글이 없습니다.</NoResultText>
+        )}
+        {!isReachingEnd && (
+          <Button
+            width="50%"
+            margin="6rem auto"
+            bgColor="brand"
+            disabled={isReachingEnd}
+            onClick={() => setSize(size + 1)}>
+            더보기
+          </Button>
+        )}
         <div ref={target} />
       </ContentWrapper>
     </Wrapper>
@@ -134,14 +102,6 @@ const Wrapper = styled.div`
 
 const ContentWrapper = styled.div`
   margin: 8rem 2.4rem 2.4rem 2.4rem;
-`;
-
-const MissingWrapper = styled.div`
-  display: ${({ isMissingActive }) => (isMissingActive ? 'block' : 'none')};
-`;
-
-const ShelterWrapper = styled.div`
-  display: ${({ isShelterActive }) => (isShelterActive ? 'block' : 'none')};
 `;
 
 const PostCardList = styled.ul`
