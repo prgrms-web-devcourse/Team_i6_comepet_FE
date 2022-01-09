92232976333ecafe89118f4914ee0cf95bbdd814
sanoopark <sanoopark@gmail.com>
2022-01-09 17:03:03 +0900

    Fix: 회원 페이지에서 로그인 리다이렉트 시 뒤로가기 문제 해결

--- src/components/AuthRoute/RequireAuth.jsx
+++ src/components/AuthRoute/RequireAuth.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { useLocation, Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import useAuth from '@/hooks/useAuth';
 
 const RequireAuth = () => {
   const { isLoggedIn } = useAuth();
-  const location = useLocation();
 
   if (!isLoggedIn) {
-    return <Navigate to="/login" state={{ from: location }} />;
+    return <Navigate to="/login" replace />;
   }
 
   return <Outlet />;
